d24609f333d72fcf696f0c6c377c72a1dea60473
sudhirn001 <218366577+emptfloortech@users.noreply.github.com>
2025-11-13 21:28:57 +0530

    Update property.module.ts

--- src/property/property.module.ts
+++ src/property/property.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { PropertyService } from './property.service';
 import { PropertyController } from './property.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
